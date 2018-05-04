794979bdb83304e067ed79e3807ad9950b6df470
AJK83 <black-market@web.de>
2018-05-04 22:07:01 +0000

    fix login highlight error

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,7 @@ router.post("/register", function(req, res){
 
 // show login form
 router.get("/login", function(req, res){
-    res.render("login", {page: "register"});
+    res.render("login", {page: "login"});
 });
 
 
